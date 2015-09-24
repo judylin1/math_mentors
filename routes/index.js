e46903ac562036500f713d1d05cd258692c6eaa4
Judy Lin <judith.s.lin@gmail.com>
2015-09-23 22:12:47 -0600

    added pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Math Mentors' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about')
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq')
+});
+
+router.get('/fees', function(req, res, next) {
+  res.render('fees')
+});
+
+router.get('/subjects', function(req, res, next) {
+  res.render('subjects')
 });
 
 module.exports = router;
